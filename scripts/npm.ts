a72e4bef08226332ec6c6eeaf412a597b9389041
Kingsword <kingsword09@gmail.com>
2023-08-08 21:42:33 +0800

    :arrow_up: upgrade dependencies

--- scripts/npm.ts
+++ scripts/npm.ts
@@ -3,10 +3,10 @@ import { build, emptyDir, type BuildOptions, type EntryPoint } from "dnt";
 import npmConfig from "./npm.json" assert { type: "json" };
 
 export const buildOptions: BuildOptions = {
-  importMap: npmConfig.importMap,
   entryPoints: npmConfig.entryPoints as EntryPoint[],
   outDir: npmConfig.outDir,
-  typeCheck: true,
+  declaration: "separate",
+  typeCheck: "both",
   shims: {
     deno: "dev",
   },
@@ -19,26 +19,21 @@ export const buildOptions: BuildOptions = {
     name: npmConfig.name,
     version: npmConfig.version,
     description: npmConfig.description,
-    license: "MIT",
-    repository: {
-      type: "git",
-      url: "git+https://github.com/BioforestChain/plaoc.git",
-    },
-    bugs: {
-      url: "https://github.com/BioforestChain/plaoc/issues",
-    },
+    license: "MIT"
   },
+  postBuild() {
+    Deno.copyFileSync(
+      path.relative(Deno.cwd(), "./LICENSE"),
+      path.relative(Deno.cwd(), path.resolve(npmConfig.outDir, "LICENSE")
+    ));
+    Deno.copyFileSync(
+      path.relative(Deno.cwd(), "./README.md"),
+      path.relative(Deno.cwd(), path.resolve(npmConfig.outDir, "README.md")
+    ));
+  }
 };
 
 if (import.meta.main) {
   emptyDir("./.npm");
   await build(buildOptions);
-  await Deno.copyFile(
-    "./LICENSE",
-    path.relative(Deno.cwd(), path.resolve(npmConfig.outDir, "LICENSE"))
-  );
-  await Deno.copyFile(
-    "./README.md",
-    path.relative(Deno.cwd(), path.resolve(npmConfig.outDir, "README.md"))
-  );
 }
